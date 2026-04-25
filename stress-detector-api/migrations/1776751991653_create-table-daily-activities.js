661a0c1aded2f6d965b34d62aded0e7400fb7965
Zetday <muhammad.rendy.07@gmail.com>
2026-04-25 12:40:41 +0800

    feat: menambah alur website

---
+++ stress-detector-api/migrations/1776751991653_create-table-daily-activities.js
@@ -0,0 +1,52 @@
+/* eslint-disable camelcase */
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('daily_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    activity_type: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    start_time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    end_time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_completed: {
+      type: 'BOOLEAN',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('daily_activities');
+};
---
+++ stress-detector-api/migrations/1776752651452_create-table-stress-prediction.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('stress_predictions', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    stress_level: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    stress_score: {
+      type: 'FLOAT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('stress_predictions');
+};
